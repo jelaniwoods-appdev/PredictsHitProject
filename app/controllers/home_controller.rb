57d4c8ce7d9acf59864ab37d34f19f562c8f301b
Thegegors <gweiss0@chicagobooth.edu>
2020-05-29 18:04:34 +0000

    added some routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
+  
   def show_homepage
     render({ :template => "home_templates/index.html.erb" })
   end
+
+  def show_about
+    render({ :template => "home_templates/about.html.erb" })
+  end
+
+  def show_support
+    render({ :template => "home_templates/support.html.erb" })
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#show_homepage'
   get("/", { :controller => "home", :action => "show_homepage" })
+  get("/about", { :controller => "home", :action => "show_about"})
+  get("/support", { :controller => "home", :action => "show_support"})
 
 end
