9c1e770c0df9b70d5761d48889cc2bae455c50a2
Thegegors <gweiss0@chicagobooth.edu>
2020-06-06 15:33:39 +0000

    Changed adding club memberships to involve entering username instead of user id

--- app/controllers/membership_controller.rb
+++ app/controllers/membership_controller.rb
@@ -2,17 +2,23 @@ class MembershipController < ApplicationController
 
   def add_club_member
     @club_id = params.fetch("club_id")
-    @member_id = params.fetch("member_id")
+    @member_username = params.fetch("username")
+    @user_row = User.where({ :username => @member_username }).at(0)
     @member_category = params.fetch("member_category")
-    
-    @new_membership = Membership.new
-    @new_membership.users_id = @member_id
-    @new_membership.clubs_id = @club_id
-    @new_membership.goes_to = "clubs_table"
-    @new_membership.category = @member_category
-    @new_membership.save
 
-    flash[:notice] = "Club Member was successfully added!" 
+    if @user_row.present?
+ 
+      @new_membership = Membership.new
+      @new_membership.users_id = @user_row.id
+      @new_membership.clubs_id = @club_id
+      @new_membership.goes_to = "clubs_table"
+      @new_membership.category = @member_category
+      @new_membership.save
+
+      flash[:notice] = "Club Member was successfully added!" 
+    else 
+      flash[:alert] = "There was an issue adding this member to your Club. Please confirm you entered the correct username."
+    end
 
     redirect_to("/clubs/manage/" + @club_id.to_s)
   end
