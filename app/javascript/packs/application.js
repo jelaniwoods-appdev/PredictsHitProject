3424ad4e9c50376ccf7af2dce36b423bb20f4923
Thegegors <gweiss0@chicagobooth.edu>
2020-06-26 22:27:31 +0000

    Updated toggle

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,6 @@ require("@rails/activestorage").start()
 //= require popper
 //= require bootstrap-sprockets
 //= require sortable-rails-jquery
-//= require_tree .
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
