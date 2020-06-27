a1e906d45d6bf0937f88abd6f3530cb15f5868b8
Thegegors <gweiss0@chicagobooth.edu>
2020-06-27 03:12:51 +0000

    added maximum username length of 15 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates_uniqueness_of :username
+  validates_presence_of :username
+  validates :username, length: { maximum: 15 }
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
