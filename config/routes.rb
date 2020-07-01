918c0f0ca354da12a17a371de061f798a6a9efcd
Thegegors <gweiss0@chicagobooth.edu>
2020-07-01 02:49:20 +0000

    continued work with closure_tree comments in Club view

--- config/routes.rb
+++ config/routes.rb
@@ -69,8 +69,6 @@ Rails.application.routes.draw do
 
   #comments
   post("/comments/club/create/:club_id/:user_id/(:parent_id)", { :controller => "comment", :action => "create_club_comment" })
-  post("/clubs/comments/club/create/:club_id/:user_id/(:parent_id)", { :controller => "comment", :action => "create_club_comment" })
-
 
   # get("/comments", { :controller => "comment", :action => "index" })
   # get("/comments/new/(:parent_id)", { :controller => "comment", :action => "new" })
