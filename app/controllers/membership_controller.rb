0335f3ded361c8ea34927f4acbce77799326ff07
Thegegors <gweiss0@chicagobooth.edu>
2020-07-14 17:35:30 +0000

    Allow Owner to change who is owner and who is admin (now for seasons as well)

--- app/controllers/membership_controller.rb
+++ app/controllers/membership_controller.rb
@@ -62,8 +62,6 @@ class MembershipController < ApplicationController
         end
       end
     end
-
-    
   end
 
 
@@ -99,6 +97,47 @@ class MembershipController < ApplicationController
 
     redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
   end
+
+    def manage_season_members
+    @club_id = params.fetch("club_id")
+    @season_id = params.fetch("season_id")
+    @member_user_id = params.fetch("user_id")
+    @user_row = User.where({ :id => @member_user_id }).at(0)
+    @membership_row = Membership.where({ :seasons_id => @season_id, :goes_to => "seasons_table", :users_id => @member_user_id }).at(0)
+    @owner_membership_row = Membership.where({ :seasons_id => @season_id, :goes_to => "seasons_table", :category => "owner" }).at(0)
+    @member_category = params.fetch("member_category")
+
+    if @member_category == @membership_row.category
+      flash[:alert] = @user_row.username.to_s + " was already a " + @member_category.to_s + "."
+      redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
+    else
+      if @member_category == "owner"
+        #if changing owner status, remove current owner and change to admin and then update user to owner
+        @owner_membership_row.category = "admin"
+        @owner_membership_row.save
+        @membership_row.category = @member_category
+        @membership_row.save
+
+        flash[:notice] = @user_row.username.to_s + " is now the " + @member_category.to_s + "."
+
+        redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
+      else
+        #do not allow owner to change themselves to admin or member
+        if @membership_row.category == "owner"
+          flash[:alert] = "You cannot remove yourself as owner without assigning someone else. If you no longer want to be the owner, assign someone else to be the owner."
+          redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
+        else
+          @membership_row.category = @member_category
+          @membership_row.save
+          
+          flash[:notice] = @user_row.username.to_s + " is now a " + @member_category.to_s + "."
+
+          redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
+        end
+      end
+    end
+  end
+
 
   def add_market_member
     @club_id = params.fetch("club_id")
