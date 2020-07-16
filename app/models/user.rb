114292b9fd075b147f64bf848fbee5fbbf62aeaa
Thegegors <gweiss0@chicagobooth.edu>
2020-07-16 16:27:33 +0000

    Updated devise to require confirmation instructions and lock out after 20 incorrect password attempts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,34 +4,50 @@
 #
 #  id                     :integer          not null, primary key
 #  category               :string
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  failed_attempts        :integer          default(0), not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  locked_at              :datetime
 #  prof_pic               :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  unconfirmed_email      :string
+#  unlock_token           :string
 #  username               :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
 # Indexes
 #
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_unlock_token          (unlock_token) UNIQUE
 #  index_users_on_username              (username) UNIQUE
 #
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   searchkick word_start: [:username]
   
   validates_uniqueness_of :username
   validates_presence_of :username
   validates :username, length: { maximum: 15 }
-  
+
+  # Include default devise modules. Others available are:
+  # :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :trackable
   
 
   has_many :memberships
