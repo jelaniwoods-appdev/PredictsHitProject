d1deb39e6700bb3699a464f83f81f5b1f1a1835e
Thegegors <gweiss0@chicagobooth.edu>
2020-08-01 16:09:55 +0000

    Added security check for updating profile details (re: profile controller)

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,17 +3,26 @@ class ProfileController < ApplicationController
   def update_profile
     @updated_username = params.fetch("updated_username")
     @user_id = params.fetch("user_id")
-    updated_profile = User.where({ :id => @user_id}).at(0)
-    updated_profile.username = @updated_username
-    if params[:prof_pic].present?
-      updated_profile.prof_pic = params.fetch("prof_pic")
-    end
-    if updated_profile.valid?
-      updated_profile.save
-      redirect_to("/profile/manage/" + @user_id, { :notice => "Your profile was successfully updated!"})
+
+    #confirm user submitting form is the user whose profile is being updated
+    if @user_id != current_user.id.to_s
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to("/")
     else
-      redirect_to("/profile/manage/" + @user_id, { :alert => "Your profile was not updated."}) #Will need to update to make more clear and be dynamic for issue once other options to change/other failure points.
+      updated_profile = User.where({ :id => @user_id}).at(0)
+      updated_profile.username = @updated_username
+      if params[:prof_pic].present?
+        updated_profile.prof_pic = params.fetch("prof_pic")
+      end
+      if updated_profile.valid?
+        updated_profile.save
+        redirect_to("/profile/manage/" + @user_id, { :notice => "Your profile was successfully updated!"})
+      else
+        redirect_to("/profile/manage/" + @user_id, { :alert => "Your profile was not updated."}) #Will need to update to make more clear and be dynamic for issue once other options to change/other failure points.
+      end
+
     end
+
   end
 
   def show_profile
