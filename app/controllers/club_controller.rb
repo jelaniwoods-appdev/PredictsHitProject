0b79ee51feb8959ec56cf18cd861cb72afa94ef7
Thegegors <gweiss0@chicagobooth.edu>
2020-08-01 01:12:07 +0000

    removed manage club in club controller since now using modal to do this

--- app/controllers/club_controller.rb
+++ app/controllers/club_controller.rb
@@ -6,22 +6,6 @@ class ClubController < ApplicationController
   end
 
 
-  def manage_club
-    club_id = params.fetch("club_id")
-    @club_row = Club.where({ :id => club_id }).at(0)
-    @season_rows = Season.where({ :club_id => club_id})
-    @membership_rows = Membership.where({ :clubs_id => club_id, :goes_to => "clubs_table"})
-    #determine owner and/or admins. Do single owner for now but later add admin info and potentially allow for multiple owners.
-    @owner_user_id = Membership.where({ :clubs_id => club_id, :goes_to => "clubs_table", :category => "owner"}).at(0).users_id
-
-    if @owner_user_id != current_user.id
-      flash[:alert] = "You are not authorized to view this page."
-      redirect_to("/")
-    else
-      render({ :template => "club_templates/manage_club_page.html.erb" })
-    end
-  end
-
   def update_club_details
 
     @updated_title = params.fetch("updated_club_title")
