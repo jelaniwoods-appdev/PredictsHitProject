d934ecfd3817b0e0168cb93a33fa20860d30d34c
Thegegors <gweiss0@chicagobooth.edu>
2020-08-01 16:00:57 +0000

    Added security check to confirm users submitting forms are club/season owner (re: membership controller)

--- app/controllers/membership_controller.rb
+++ app/controllers/membership_controller.rb
@@ -6,23 +6,34 @@ class MembershipController < ApplicationController
     @user_row = User.where("lower(username) = ?", @member_username.downcase).at(0)
     @member_category = params.fetch("member_category")
 
-    if @user_row.present?
- 
-      @new_membership = Membership.new
-      @new_membership.users_id = @user_row.id
-      @new_membership.clubs_id = @club_id
-      @new_membership.goes_to = "clubs_table"
-      @new_membership.category = @member_category
-      if @new_membership.valid?
-        @new_membership.save
-        flash[:notice] = "Club Member was successfully added!" 
-      else
-        flash[:alert] = "It looks like this user is already a member!" 
+    #confirm user submitting form is the club owner
+    @owner_user_id = Membership.where({ :clubs_id => @club_id, :goes_to => "clubs_table", :category => "owner"}).at(0).users_id
+
+    if @owner_user_id != current_user.id
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to("/clubs/" + @club_id.to_s)
+    else
+        
+      if @user_row.present?
+  
+        @new_membership = Membership.new
+        @new_membership.users_id = @user_row.id
+        @new_membership.clubs_id = @club_id
+        @new_membership.goes_to = "clubs_table"
+        @new_membership.category = @member_category
+        if @new_membership.valid?
+          @new_membership.save
+          flash[:notice] = "Club Member was successfully added!" 
+        else
+          flash[:alert] = "It looks like this user is already a member!" 
+        end
+      else 
+        flash[:alert] = "There was an issue adding this member to your Club. Please confirm you entered the correct username."
       end
-    else 
-      flash[:alert] = "There was an issue adding this member to your Club. Please confirm you entered the correct username."
+      redirect_to("/clubs/" + @club_id.to_s)
+
     end
-    redirect_to("/clubs/" + @club_id.to_s)
+
   end
 
   def manage_club_members
@@ -31,37 +42,47 @@ class MembershipController < ApplicationController
     @user_row = User.where({ :id => @member_user_id }).at(0)
     @membership_row = Membership.where({ :clubs_id => @club_id, :goes_to => "clubs_table", :users_id => @member_user_id }).at(0)
     @owner_membership_row = Membership.where({ :clubs_id => @club_id, :goes_to => "clubs_table", :category => "owner" }).at(0)
+    @owner_user_id = @owner_membership_row.users_id
     @member_category = params.fetch("member_category")
 
-    if @member_category == @membership_row.category
-      flash[:alert] = @user_row.username.to_s + " was already a " + @member_category.to_s + "."
+    #confirm user submitting form is the club owner
+    if @owner_user_id != current_user.id
+      flash[:alert] = "You are not authorized to perform this action."
       redirect_to("/clubs/" + @club_id.to_s)
     else
-      if @member_category == "owner"
-        #if changing owner status, remove current owner and change to admin and then update user to owner
-        @owner_membership_row.category = "admin"
-        @owner_membership_row.save
-        @membership_row.category = @member_category
-        @membership_row.save
 
-        flash[:notice] = @user_row.username.to_s + " is now the " + @member_category.to_s + "."
-
+      if @member_category == @membership_row.category
+        flash[:alert] = @user_row.username.to_s + " was already a " + @member_category.to_s + "."
         redirect_to("/clubs/" + @club_id.to_s)
       else
-        #do not allow owner to change themselves to admin or member
-        if @membership_row.category == "owner"
-          flash[:alert] = "You cannot remove yourself as owner without assigning someone else. If you no longer want to be the owner, assign someone else to be the owner."
-          redirect_to("/clubs/" + @club_id.to_s)
-        else
+        if @member_category == "owner"
+          #if changing owner status, remove current owner and change to admin and then update user to owner
+          @owner_membership_row.category = "admin"
+          @owner_membership_row.save
           @membership_row.category = @member_category
           @membership_row.save
-          
-          flash[:notice] = @user_row.username.to_s + " is now a " + @member_category.to_s + "."
+
+          flash[:notice] = @user_row.username.to_s + " is now the " + @member_category.to_s + "."
 
           redirect_to("/clubs/" + @club_id.to_s)
+        else
+          #do not allow owner to change themselves to admin or member
+          if @membership_row.category == "owner"
+            flash[:alert] = "You cannot remove yourself as owner without assigning someone else. If you no longer want to be the owner, assign someone else to be the owner."
+            redirect_to("/clubs/" + @club_id.to_s)
+          else
+            @membership_row.category = @member_category
+            @membership_row.save
+            
+            flash[:notice] = @user_row.username.to_s + " is now a " + @member_category.to_s + "."
+
+            redirect_to("/clubs/" + @club_id.to_s)
+          end
         end
       end
+
     end
+
   end
 
   def leave_club
@@ -110,31 +131,41 @@ class MembershipController < ApplicationController
     @member_id = params.fetch("member_id")
     @member_category = params.fetch("member_category")
     
-    #add validation check!?
+    #confirm user submitting form is the season owner
+    @owner_user_id = Membership.where({ :seasons_id => @season_id, :goes_to => "seasons_table", :category => "owner"}).at(0).users_id
     
-    #Create membership row for added user
-    @new_membership = Membership.new
-    @new_membership.users_id = @member_id
-    @new_membership.clubs_id = @club_id
-    @new_membership.seasons_id = @season_id
-    @new_membership.goes_to = "seasons_table"
-    @new_membership.category = @member_category
-    @new_membership.save
-
-    #Add asset row for added user
-      #find fund row in season table
-      @season_fund = Season.where({ :id => @season_id }).at(0).fund
+    if @owner_user_id != current_user.id
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
+    else
     
-    @new_membership_asset = Asset.new
-    @new_membership_asset.membership_id = @new_membership.id
-    @new_membership_asset.season_id = @season_id
-    @new_membership_asset.category = "season_fund"
-    @new_membership_asset.quantity = @season_fund
-    @new_membership_asset.save
+      #Create membership row for added user
+      @new_membership = Membership.new
+      @new_membership.users_id = @member_id
+      @new_membership.clubs_id = @club_id
+      @new_membership.seasons_id = @season_id
+      @new_membership.goes_to = "seasons_table"
+      @new_membership.category = @member_category
+      @new_membership.save
 
-    flash[:notice] = "Season Member was successfully added!" 
+      #Add asset row for added user
+        #find fund row in season table
+        @season_fund = Season.where({ :id => @season_id }).at(0).fund
+      
+      @new_membership_asset = Asset.new
+      @new_membership_asset.membership_id = @new_membership.id
+      @new_membership_asset.season_id = @season_id
+      @new_membership_asset.category = "season_fund"
+      @new_membership_asset.quantity = @season_fund
+      @new_membership_asset.save
 
-    redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
+      flash[:notice] = "Season Member was successfully added!" 
+
+      redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
+
+
+    end
+
   end
 
     def manage_season_members
@@ -144,37 +175,48 @@ class MembershipController < ApplicationController
     @user_row = User.where({ :id => @member_user_id }).at(0)
     @membership_row = Membership.where({ :seasons_id => @season_id, :goes_to => "seasons_table", :users_id => @member_user_id }).at(0)
     @owner_membership_row = Membership.where({ :seasons_id => @season_id, :goes_to => "seasons_table", :category => "owner" }).at(0)
+    @owner_user_id = @owner_membership_row.users_id
     @member_category = params.fetch("member_category")
 
-    if @member_category == @membership_row.category
-      flash[:alert] = @user_row.username.to_s + " was already a " + @member_category.to_s + "."
+    #confirm user submitting form is the season owner
+    
+    if @owner_user_id != current_user.id
+      flash[:alert] = "You are not authorized to perform this action."
       redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
     else
-      if @member_category == "owner"
-        #if changing owner status, remove current owner and change to admin and then update user to owner
-        @owner_membership_row.category = "admin"
-        @owner_membership_row.save
-        @membership_row.category = @member_category
-        @membership_row.save
 
-        flash[:notice] = @user_row.username.to_s + " is now the " + @member_category.to_s + "."
-
+      if @member_category == @membership_row.category
+        flash[:alert] = @user_row.username.to_s + " was already a " + @member_category.to_s + "."
         redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
       else
-        #do not allow owner to change themselves to admin or member
-        if @membership_row.category == "owner"
-          flash[:alert] = "You cannot remove yourself as owner without assigning someone else. If you no longer want to be the owner, assign someone else to be the owner."
-          redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
-        else
+        if @member_category == "owner"
+          #if changing owner status, remove current owner and change to admin and then update user to owner
+          @owner_membership_row.category = "admin"
+          @owner_membership_row.save
           @membership_row.category = @member_category
           @membership_row.save
-          
-          flash[:notice] = @user_row.username.to_s + " is now a " + @member_category.to_s + "."
+
+          flash[:notice] = @user_row.username.to_s + " is now the " + @member_category.to_s + "."
 
           redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
+        else
+          #do not allow owner to change themselves to admin or member
+          if @membership_row.category == "owner"
+            flash[:alert] = "You cannot remove yourself as owner without assigning someone else. If you no longer want to be the owner, assign someone else to be the owner."
+            redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
+          else
+            @membership_row.category = @member_category
+            @membership_row.save
+            
+            flash[:notice] = @user_row.username.to_s + " is now a " + @member_category.to_s + "."
+
+            redirect_to("/seasons/" + @club_id.to_s + "/" + @season_id.to_s)
+          end
         end
       end
+
     end
+
   end
 
   def leave_season
@@ -206,56 +248,64 @@ class MembershipController < ApplicationController
     @member_username = params.fetch("username")
     @user_row = User.where("lower(username) = ?", @member_username.downcase).at(0)
     
-    #add validation check!?
+    #confirm user submitting form is both the season owner and the club owner
+    @club_owner_user_id = Membership.where({ :clubs_id => @club_id, :goes_to => "clubs_table", :category => "owner"}).at(0).users_id
+    @season_owner_user_id = Membership.where({ :seasons_id => @season_id, :goes_to => "seasons_table", :category => "owner"}).at(0).users_id
     
-    #Create club membership row for added user
-    if @user_row.present?
-
-      @new_club_membership = Membership.new
-      @new_club_membership.users_id = @user_row.id
-      @new_club_membership.clubs_id = @club_id
-      @new_club_membership.goes_to = "clubs_table"
-      @new_club_membership.category = "member"
-      if @new_club_membership.valid?
-        @new_club_membership.save
-      end
-
-      #Create season membership row for added user
-      @new_season_membership = Membership.new
-      @new_season_membership.users_id = @user_row.id
-      @new_season_membership.clubs_id = @club_id
-      @new_season_membership.seasons_id = @season_id
-      @new_season_membership.goes_to = "seasons_table"
-      @new_season_membership.category = "member"
-      if @new_season_membership.valid?
-        @new_season_membership.save
-
-        #Add asset row for added user
-          #find fund row in season table
-          @season_fund = Season.where({ :id => @season_id }).at(0).fund
-        
-        @new_season_membership_asset = Asset.new
-        @new_season_membership_asset.membership_id = @new_season_membership.id
-        @new_season_membership_asset.season_id = @season_id
-        @new_season_membership_asset.category = "season_fund"
-        @new_season_membership_asset.quantity = @season_fund
-        @new_season_membership_asset.save
-
-        flash[:notice] = "Player was successfully added!" 
-
-        redirect_to("/markets/" + @club_id.to_s + "/" + @season_id.to_s + "/" + @market_id.to_s)
-
-      else
-        flash[:alert] = "There was an issue adding this player. It looks like the player has already been invited!" 
-
-        redirect_to("/markets/" + @club_id.to_s + "/" + @season_id.to_s + "/" + @market_id.to_s)
-      end
-      
-    else 
-      flash[:alert] = "There was an issue adding this player. Please confirm you entered the correct username."
+    if (@club_owner_user_id != current_user.id) || (@season_owner_user_id != current_user.id)
+      flash[:alert] = "You are not authorized to perform this action."
       redirect_to("/markets/" + @club_id.to_s + "/" + @season_id.to_s + "/" + @market_id.to_s)
-    end
+    else
 
+      #Create club membership row for added user
+      if @user_row.present?
+
+        @new_club_membership = Membership.new
+        @new_club_membership.users_id = @user_row.id
+        @new_club_membership.clubs_id = @club_id
+        @new_club_membership.goes_to = "clubs_table"
+        @new_club_membership.category = "member"
+        if @new_club_membership.valid?
+          @new_club_membership.save
+        end
+
+        #Create season membership row for added user
+        @new_season_membership = Membership.new
+        @new_season_membership.users_id = @user_row.id
+        @new_season_membership.clubs_id = @club_id
+        @new_season_membership.seasons_id = @season_id
+        @new_season_membership.goes_to = "seasons_table"
+        @new_season_membership.category = "member"
+        if @new_season_membership.valid?
+          @new_season_membership.save
+
+          #Add asset row for added user
+            #find fund row in season table
+            @season_fund = Season.where({ :id => @season_id }).at(0).fund
+          
+          @new_season_membership_asset = Asset.new
+          @new_season_membership_asset.membership_id = @new_season_membership.id
+          @new_season_membership_asset.season_id = @season_id
+          @new_season_membership_asset.category = "season_fund"
+          @new_season_membership_asset.quantity = @season_fund
+          @new_season_membership_asset.save
+
+          flash[:notice] = "Player was successfully added!" 
+
+          redirect_to("/markets/" + @club_id.to_s + "/" + @season_id.to_s + "/" + @market_id.to_s)
+
+        else
+          flash[:alert] = "There was an issue adding this player. It looks like the player has already been invited!" 
+
+          redirect_to("/markets/" + @club_id.to_s + "/" + @season_id.to_s + "/" + @market_id.to_s)
+        end
+        
+      else 
+        flash[:alert] = "There was an issue adding this player. Please confirm you entered the correct username."
+        redirect_to("/markets/" + @club_id.to_s + "/" + @season_id.to_s + "/" + @market_id.to_s)
+      end
+
+    end
   end
 
 end
