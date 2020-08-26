235f072ec07a492549d5be1874ae62b16f9f598c
Thegegors <gweiss0@chicagobooth.edu>
2020-08-26 16:10:35 +0000

    Updated room_channel.rb to account for memberships only applying to season and club and not market

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,14 +2,23 @@ class RoomChannel < ApplicationCable::Channel
   def subscribed
     return false unless %w[market season club].include?(params[:room_type])
 
-    membership = current_user.memberships.where(
-      goes_to: "#{params[:room_type]}s_table", 
-      "#{params[:room_type]}" => params[:room_id]
-    ).first
+    if params[:room_type] == "club" || params[:room_type] == "season"
+      membership = current_user.memberships.where(
+        goes_to: "#{params[:room_type]}s_table", 
+        "#{params[:room_type]}" => params[:room_id]
+      ).first
+    elsif params[:room_type] == "market"
+      associated_season_id = Market.where(id: params[:room_id]).first.season_id
+      membership = current_user.memberships.where(
+        goes_to: "seasons_table", 
+        "season" => associated_season_id
+      ).first
 
+    end
+    
     return false unless membership.present?
-
     stream_from "room_channel_#{params[:room_type]}_#{params[:room_id]}"
+
   end
 
   def unsubscribed
